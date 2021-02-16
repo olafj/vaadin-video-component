540a40cfef6400216032748154c7251b269a9433
Olaf Jantz <olaf.jantz@appcreation.de>
2021-02-16 16:59:30 +0100

    test resources

--- src/test/java/com/github/olafj/View.java
+++ src/test/java/com/github/olafj/View.java
@@ -4,11 +4,19 @@ import com.github.olafj.vaadin.flow.Video;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.Route;
 
+import java.nio.file.Paths;
+
 @Route("")
 public class View extends Div {
 
+
     public View() {
         Video video = new Video();
+        video.setWidth("540px");
+        video.setControls(true);
+        video.setSource(Paths.get("./target/test-classes/sample.mp4"));
+        video.setPosterSource(Paths.get("./target/test-classes/cover.jpeg"));
+        video.getStyle().set("object-fit", "cover");
         add(video);
     }
 }
